93c202f2282326eaa4faf479c54a47fb55c775fb
YJ <good.dba.yj@gmail.com>
2016-07-11 19:09:49 +0900

    스키마명 제거
    테이블 생성문에 포함돼 있던 스키마명 제거

--- tables/workload_meta_locks.sql
+++ tables/workload_meta_locks.sql
@@ -3,16 +3,16 @@
 # Date  : 2016.07.08
 # Desc  : Metadata Lock Snapshot
 #
-drop table if exists sys.workload_meta_locks;
+drop table if exists workload_meta_locks;
 
-create table sys.workload_meta_locks
+create table workload_meta_locks
 (
 snap_id int unsigned not null comment 'snapshot id',
 thread_id     bigint unsigned not null comment 'MySQL thread ID. Can be used for joining with PROCESSLIST on ID',
 user_host     varchar(200) comment 'user@host',
 lock_mode     varchar(24) comment 'One of MDL_INTENTION_EXCLUSIVE, MDL_SHARED, MDL_SHARED_HIGH_PRIO, MDL_SHARED_READ, MDL_SHARED_WRITE, MDL_SHARED_NO_WRITE, MDL_SHARED_NO_READ_WRITE or MDL_EXCLUSIVE.',
 lock_duration varchar(30) comment 'One of MDL_STATEMENT, MDL_TRANSACTION or MDL_EXPLICIT',
-lock_type     varchar(30) comment '	One of Global read lock, Schema metadata lock, Table metadata lock, Stored function metadata lock, Stored procedure metadata lock, Trigger metadata lock, Event metadata lock, Commit lock or User lock.',
+lock_type     varchar(30) comment ' One of Global read lock, Schema metadata lock, Table metadata lock, Stored function metadata lock, Stored procedure metadata lock, Trigger metadata lock, Event metadata lock, Commit lock or User lock.',
 table_schema  varchar(64) comment '',
 table_name    varchar(64) comment '',
 primary key (snap_id, thread_id, lock_type, table_schema, table_name)
