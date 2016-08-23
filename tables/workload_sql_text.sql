b790082b60351c8f0ac5ddf3dfaee9af37073f7d
YJ <good.dba.yj@gmail.com>
2016-08-23 11:10:08 +0900

    "digest sql text"

--- tables/workload_sql_text.sql
+++ tables/workload_sql_text.sql
@@ -1,15 +1,18 @@
 #
+# Name: workload_sql_text
 # Author: YJ
-# Date  : 2016.07.11
-# Desc  : keep "digest sql text" (digest: a normalized form of a statement)
+# Created : 2016.07.11
+# Last Updated: 2016.08.22
+# Desc: "digest sql text"
+#       (digest: a normalized form of a statement)
 #
 drop table if exists workload_sql_text;
 
 create table workload_sql_text
 (
-schema_name                 varchar(64)         not null comment 'Database name. Records are summarised together with DIGEST.',
-digest                      varchar(32)         not null comment 'Performance Schema digest. (digest: a normalized form of a statement) Records are summarised together with SCHEMA NAME.',
-digest_text                 longtext            comment 'The unhashed form of the digest.',
+schema_name varchar(64) not null comment 'Database name. Records are summarised together with DIGEST.',
+digest      varchar(32) not null comment 'Performance Schema digest. (digest: a normalized form of a statement) Records are summarised together with SCHEMA NAME.',
+digest_text longtext             comment 'The unhashed form of the digest.',
 primary key (digest, schema_name)
 )
 engine = 'MyISAM'
@@ -17,3 +20,4 @@ charset = 'utf8'
 collate = 'utf8_general_ci'
 comment = 'DB workload - sql digest(a normalized form of a statement)'
 ;
+
