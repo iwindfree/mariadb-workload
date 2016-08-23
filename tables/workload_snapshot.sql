4d81b2c41b8489839c45fb784d640e42108643a8
YJ <good.dba.yj@gmail.com>
2016-08-23 11:09:22 +0900

    DB workload snapshot history

--- tables/workload_snapshot.sql
+++ tables/workload_snapshot.sql
@@ -1,17 +1,19 @@
 #
+# Name: workload_snapshot
 # Author: YJ
-# Date  : 2016.07.08
-# Desc  : snapshot history
+# Created : 2016.07.08
+# Last Updated: 2016.08.22
+# Desc: DB workload snapshot history
 #
 drop table if exists workload_snapshot;
 
 create table workload_snapshot
 (
-snap_id int unsigned auto_increment not null comment 'snapshot id',
-begin_snap_time datetime not null default current_timestamp() comment 'snap start time',
-end_snap_time datetime comment 'snap end time',
-state varchar(10) not null comment 'RUNNING,COMPLETED,ERRORED',
-err_msg varchar(1024) comment 'error messages are recorded only when there is an exception.',
+snap_id         int unsigned  auto_increment not null comment 'snapshot id',
+begin_snap_time datetime      not null default current_timestamp() comment 'snap start time',
+end_snap_time   datetime               comment 'snap end time',
+state           varchar(10)   not null comment 'RUNNING,COMPLETED,ERRORED',
+err_msg         varchar(1024)          comment 'error messages are recorded only when there was errors.',
 primary key (snap_id),
 index ix_begin_snap_time (begin_snap_time)
 )
@@ -20,3 +22,4 @@ charset = 'utf8'
 collate = 'utf8_general_ci'
 comment = 'DB workload - setting for snapshot'
 ;
+
