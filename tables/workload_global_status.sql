407ea5b0fd2f0b5cd863e49ba720cc63d83ac21c
YJ <good.dba.yj@gmail.com>
2016-08-23 11:07:10 +0900

    DB Global Status Snapshot

--- tables/workload_global_status.sql
+++ tables/workload_global_status.sql
@@ -1,15 +1,17 @@
 #
+# Name: workload_global_status
 # Author: YJ
-# Date  : 2016.07.08
-# Desc  : Global Status Snapshot
+# Created : 2016.07.08
+# Last Updated: 2016.08.23
+# Desc: Global Status Snapshot
 #
 drop table if exists workload_global_status;
 
 create table workload_global_status
 (
-snap_id int unsigned not null comment 'snapshot id',
-variable_name varchar(64) not null,
-variable_value varchar(2048) not null,
+snap_id        int unsigned  not null comment 'snapshot id',
+variable_name  varchar(64)   not null comment 'status name',
+variable_value varchar(2048) not null comment 'status value',
 primary key (snap_id, variable_name)
 )
 engine = 'MyISAM'
