52f5b8259552ead0cd33e4b3c2f2e9473c061e17
YJ <good.dba.yj@gmail.com>
2016-08-23 11:09:04 +0900

    Settings for Gather DB workload

--- tables/workload_setup.sql
+++ tables/workload_setup.sql
@@ -1,15 +1,17 @@
 #
+# Name: workload_setup
 # Author: YJ
-# Date  : 2016.07.08
-# Desc  : setting value for gather db workload
+# Created : 2016.07.08
+# Last Updated: 2016.08.22
+# Desc: Settings for Gather DB workload
 #
 drop table if exists workload_setup;
 
 create table workload_setup
 (
-expire_snapshot_days int unsigned not null comment 'days to retain snapshots',
-primary key (expire_snapshot_days)
-)
+ expire_snapshot_days int unsigned not null comment 'days to retain snapshots',
+ primary key (expire_snapshot_days)
+ )
 engine = 'MyISAM'
 charset = 'utf8'
 collate = 'utf8_general_ci'
@@ -17,3 +19,4 @@ comment = 'DB workload - setting for snapshot'
 ;
 
 insert into workload_setup (expire_snapshot_days) values (7);
+
